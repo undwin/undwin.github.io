71738e04736b3403c6a6a600af598a4f83d450cb
KuoFane <KuoFane@163.com>
2023-02-10 12:32:40 +0800

    'config'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,16 +8,6 @@ export default defineUserConfig({
   description: '这是我的第一个 VuePress 站点',
   deion:'基于 umi 的 Ant Design 实战教程',
   plugins:[
-     backToTopPlugin(),
-      ['vuepress-plugin-yuque', {
-      repoUrl: 'https://www.yuque.com/muuc/blog',
-      authToken:'Enaz3HXg2cM55in5DJ64W32kedkf6r1suImoXghc',
-      home: {
-        actionText: 'Getting Started →',
-        actionLink: '/intro/',
-        heroImage: 'https://cdn.nlark.com/yuque/0/2018/png/84868/1535520500482-avatar/20c595c5-ab31-4543-9142-f36cc87c8868.png?x-oss-process=image/resize,m_fill,w_320,h_320',
-        footer: `Copyright © 蚂蚁金服体验技术部`
-      }
-    }]
+     backToTopPlugin()
     ]
 })

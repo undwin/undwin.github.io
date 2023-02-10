2e8a27fe08b4bda33efab58eb1b1d7afcfa63b8b
KuoFane <KuoFane@163.com>
2023-02-10 10:18:09 +0800

    '.vuepress'

---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1675995128000,\"contributors\":[{\"name\":\"KuoFane\",\"email\":\"KuoFane@163.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}

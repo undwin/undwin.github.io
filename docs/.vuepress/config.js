4fbb146f37e1155acf57a72211575f68aee33d19
KuoFane <KuoFane@163.com>
2023-02-10 12:19:33 +0800

    'config'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,10 +7,10 @@ export default {
   lang: 'zh-CN',
   theme:defaultTheme(),
   title: '你好， VuePress ！',
+  base: '/undwin.github.io/',
   description: '这是我的第一个 VuePress 站点',
   deion:'基于 umi 的 Ant Design 实战教程',
   plugins:[
-    
      backToTopPlugin(),
       // 搜索插件
     searchPlugin({

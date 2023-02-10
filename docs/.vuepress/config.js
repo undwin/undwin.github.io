057b5f295cab075292e6012c6b6e17d0a99acfef
KuoFane <KuoFane@163.com>
2023-02-10 13:53:08 +0800

    'config'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
+  title: '你好， VuePress1 ！',
   base: '/undwin.github.io/',
   description: '这是我的第一个 VuePress 站点',
   deion:'基于 umi 的 Ant Design 实战教程',

1c11a593f7b2f50ecc7980fe9f1b50dd6166c4dc
KuoFane <KuoFane@163.com>
2023-02-10 16:12:01 +0800

    'config.js'

---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,14 @@
+export const siteData = JSON.parse("{\"base\":\"/undwin.github.io/\",\"lang\":\"zh-CN\",\"title\":\"\",\"description\":\"开发技术，分布式，微服务，高并发，高可用，高可扩展，高可维护，JVM技术，MySQL，分布式数据库，分布式事务，云原生，大数据，云计算，渗透技术，各种面试题，面试技巧\",\"head\":[],\"locales\":{}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,36 @@
-import { defaultTheme,defineUserConfig } from 'vuepress'
 import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
 
-export default defineUserConfig({
+// .vuepress/config.js
+module.exports = {
+  title: '',
   lang: 'zh-CN',
-  title: '你好， VuePress1 ！',
   base: '/undwin.github.io/',
-  description: '这是我的第一个 VuePress 站点',
-  deion:'基于 umi 的 Ant Design 实战教程',
-  plugins:[
+  description: '开发技术，分布式，微服务，高并发，高可用，高可扩展，高可维护，JVM技术，MySQL，分布式数据库，分布式事务，云原生，大数据，云计算，渗透技术，各种面试题，面试技巧',
+  markdown:{
+      pageSuffix: '.md',
+  },
+  themeConfig: {
+       displayAllHeaders: true ,
+       activeHeaderLinks: false,
+       smoothScroll: true,
+       sidebarDepth: 2,
+       nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: '消息中间件', 
+                items: [
+                    // { text: 'kafka', link: '/kafka/' }
+                ]
+            }
+        ],
+        sidebar: {
+             // '/kafka/': genSidebar.genDefaultSidebar('kafka', 'kafka', true, 4),
+             '/': ['']
+
+        }
+  },
+    plugins:[
      backToTopPlugin()
     ]
-})
+
+}

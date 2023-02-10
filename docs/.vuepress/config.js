d9facf4a27e94a5b9f386a1a68e2c97c76c60e68
KuoFane <KuoFane@163.com>
2023-02-10 10:47:21 +0800

    'config'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,4 +5,10 @@ export default defineUserConfig({
   title: '你好， VuePress ！',
   base: '/undwin.github.io/',
   description: '这是我的第一个 VuePress 站点',
+  deion:'基于 umi 的 Ant Design 实战教程',
+  plugins:[
+      ['vuepress-plugin-yuque', {
+      repoUrl: 'https://www.yuque.com/muuc/blog',
+    }]
+    ]
 })
